d48c611b8d8d140a4639bebdeca58244a0f3aa54
xuzhiwei <xuzhiwei@xiaomi.com>
2016-07-18 18:06:50 +0800

    update

---
+++ src/main/java/test/SpringMVC/Hello.java
@@ -0,0 +1,18 @@
+package test.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/fcoupon")
+public class Hello {
+	
+	@RequestMapping("/say")
+	@ResponseBody
+	public String say()
+	{
+		return "hello world master";
+	}
+
+}
